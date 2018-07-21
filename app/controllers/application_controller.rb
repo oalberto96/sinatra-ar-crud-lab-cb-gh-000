693a3faa1983dadbda991584ee2a6862b30b7b7a
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-21 04:44:37 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,46 @@ class ApplicationController < Sinatra::Base
   get '/' do
 
   end
+
+  get'/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    Post.create(params)
+    erb :index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id].to_i)
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id].to_i)
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    new_data = {
+      name: params[:name],
+      content: params[:content]
+    }
+    Post.update(params[:id].to_i,new_data)
+    erb :index
+  end
+
+  delete '/posts/:id/delete' do
+    Post.destroy(params[:id].to_i)
+    erb :index
+  end
+
+
+
+
 end
